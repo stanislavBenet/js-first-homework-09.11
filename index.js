8fffe12edd35748b49f1eeab4d791fda67e5f7cf
stanislavBenet <stanislav.benet@gmail.com>
2022-11-10 20:15:07 +0200

    Add correct version homework

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 let userInput = prompt("Enter number");
-if (chekInputIsNumber(Number(userInput))) {
-  if (isNumberOddOrEven(Number(userInput))) {
+
+function isNumberOddOrEven(val) {
+  return val % 2 === 0;
+}
+
+const showNumberIsEvenOrOdd = function (val) {
+  if (isNumberOddOrEven(val)) {
     console.log("even");
   } else {
     console.log("odd");
   }
-}
+};
 
-function isNumberOddOrEven(val) {
-  return val% 2 === 0;
-}
-
-function chekInputIsNumber(val) {
-  if (isNaN(val) || val === null || val === "") {
-    return console.log('ONLY NUMBER!!!');
-  }
-   return true;
-}
